a655ad9955182bfc6555831e6288b71d032dd0fb
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2018-02-01 14:24:48 +0530

    Update hrminsub.c

--- hrminsub.c
+++ hrminsub.c
@@ -1,12 +1,16 @@
-#include<stdio.h>
-int main()
-{
-	int h1,m1,h2,m2,h3,m3;
-	scanf("%d%d%d%d",&h1,&m1,&h2,&m2);
-	
+#include <stdio.h>
+
+int main(void) {
+	int h1,m1,h2,m2,h3,m3,s,a;
+	scanf("%d\t%d\n%d\t%d",&h1,&m1,&h2,&m2);
 	h3=h1-h2;
 	m3=m1-m2;
-	
-	printf(" %d\t%d",h3,m3);
+	s=h3*60;
+	if(s<0)
+	a=-(s+m3);
+	else
+	a=-(s-m3);
+	printf("difference in time is %d:%d",a/60,a%60);
+	// your code goes here
 	return 0;
 }
