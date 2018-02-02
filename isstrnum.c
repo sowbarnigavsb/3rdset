c4d79e60d3973b9c6174445664ffc8218418a454
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2018-02-02 14:14:44 +0530

    Update isstrnum.c

--- isstrnum.c
+++ isstrnum.c
@@ -4,10 +4,9 @@ int main(void) {
 	char s[50];
 	int c=0,i;
 	scanf("%s",&s);
-	
             for(i=0;i<='\0';i++)
 	{
-	if((s[i]>=0)&&(s[i]<=9))
+	if((s[i]>='0')&&(s[i]<='9'))
             c++;
 	}
 	if(c>=1)
